44cf0c32e9f3d91ab00bded640f916981ae0d065
Edip SACİ <saciedip@gmail.com>
2022-12-25 11:37:13 +0300

    week 16 final keyword

---
+++ src/week16_12_24_2022/final_keyword/final_with_variable/Person.java
@@ -0,0 +1,28 @@
+package week16_12_24_2022.final_keyword.final_with_variable;
+
+public class Person {
+
+    public String name;
+    final public String BIRTHDAY;
+
+    public Person(String BIRTHDAY,String name) {
+        this.BIRTHDAY = BIRTHDAY;
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", birthDay='" + BIRTHDAY + '\'' +
+                '}';
+    }
+}
---
+++ src/week16_12_24_2022/final_keyword/final_with_variable/TaskPerson.java
@@ -0,0 +1,10 @@
+package week16_12_24_2022.final_keyword.final_with_variable;
+
+public class TaskPerson {
+    public static void main(String[] args) {
+        Person person= new Person("14-01-1991","John");
+
+        //person.birthDay="12-01-1997"; we can not change the birthday. because its final
+
+    }
+}
